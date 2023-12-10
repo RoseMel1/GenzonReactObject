c7452b72f68afef084acfc3ac54458e17898e0f7
JunreyG <143070037+Valientemelrose@users.noreply.github.com>
2023-12-10 11:09:45 +0800

    initial change

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Users } from "./src/Users";
+import { FlatList } from "react-native";
+import Toast from 'react-native-toast-message';
+
+
 
 export default function App() {
+  const displayToast = (user) => {
+    Toast.show({
+      type: 'success',
+      text1: `Hello ${user.name.firstname} ${user.name.lastname} from ${user.course} 3B`,
+      text2: 'HAPPY BIRTHDAY',
+      position: 'bottom',
+    });
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headtext}>LIST OF USERS</Text>
+      <View style={styles.table}>
+        <View style={styles.tables}>
+          <Text style={styles.tabletexts}>ID</Text>
+        </View>
+        <View style={styles.tables}>
+          <Text style={styles.tabletexts}>NAME</Text>
+        </View>
+        <View style={styles.tables}>
+          <Text style={styles.tabletexts}>COURSE</Text>
+        </View>
+      </View>
+      <FlatList
+        data={Users}
+        renderItem={({ item: user }) => (
+          <TouchableOpacity
+            style={styles.tablerow}
+            onPress={() => displayToast(user)}
+          >
+            <Text style={styles.tabletext}>{user.id}</Text>
+            <Text style={styles.tabletext}>{user.name.firstname}</Text>
+            <Text style={styles.tabletext}>{user.course}</Text>
+            </TouchableOpacity>
+           
+        )}
+        keyExtractor={(user) => user.id}
+      />
+      
+    <Toast ref={(ref) => Toast.setRef(ref)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +56,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 30,
+    paddingHorizontal: 5
   },
+  table: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    borderColor: 'black',
+    borderWidth: 0.9,
+    backgroundColor: '#61DBFB'
+  },
+  tablerow: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    borderColor: '#61DBFB',
+    borderBottomWidth: 0.9,
+    borderLeftWidth: 0.9,
+    borderRightWidth: 0.9
+  },
+  tabletext: {
+    fontSize: 25
+  },
+  tabletexts: {
+    fontSize: 30,
+    color: 'white'
+  },
+  headtext: {
+    fontSize: 30,
+    margin: 10,
+    alignSelf: 'center'
+  }
 });
---
+++ src/Users.js
@@ -0,0 +1,82 @@
+export const Users = [
+    {
+      id: 1,
+      name: {
+        firstname: "Junrey",
+        lastname: "Genzon",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "Rodel Glenn",
+        lastname: "Jawa",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "John Mark",
+        lastname: "Lagrimas",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "Jess",
+        lastname: "Jumawan",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "Jovert",
+        lastname: "Davirao",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "John Carlo",
+        lastname: "Razon",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "Aldwin",
+        lastname: "Handumon",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "James",
+        lastname: "Noble",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "Jhannel",
+        lastname: "Quisquirin",
+      },
+      course: "BSIT",
+    },
+    {
+      id: 10,
+      name: {
+        firstname: "Karl",
+        lastname: "Loquias",
+      },
+      course: "BSIT",
+    },
+  ];
